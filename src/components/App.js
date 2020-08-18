0601157ba5ae4bafa872b9750ee39e488eb1c972
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-08-17 21:14:18 -0400

    ex 3 mod Fetch and Reducer function

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,20 @@ import { SeatContext } from "./SeatContext";
 
 function App() {
   const {
-    actions: { receiveSeatIinfoFromServer },
+    state: { numOfRows },
+    actions: { receiveSeatInfoFromServer },
   } = React.useContext(SeatContext);
 
   React.useEffect(() => {
     fetch("/api/seat-availability")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => receiveSeatInfoFromServer(data));
   }, []);
 
   return (
     <>
       <GlobalStyles />
-      TODO: write code
+      We currently have {numOfRows} rows!
     </>
   );
 }

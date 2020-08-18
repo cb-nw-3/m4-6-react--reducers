68d779263a9c973785e71550ab77ae5f7e9c38bc
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-08-17 20:50:42 -0400

    started ex3

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
-
-import GlobalStyles from './GlobalStyles';
+import React from "react";
+import GlobalStyles from "./GlobalStyles";
+import { SeatContext } from "./SeatContext";
 
 function App() {
+  const {
+    actions: { receiveSeatIinfoFromServer },
+  } = React.useContext(SeatContext);
+
+  React.useEffect(() => {
+    fetch("/api/seat-availability")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <>
       <GlobalStyles />

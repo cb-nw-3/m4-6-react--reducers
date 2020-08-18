aa0eeb93e6202f1ae86d9d67b58b56cd8bea471d
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-08-17 22:35:29 -0400

    started ex5, spinner done, no gray no tip

--- src/components/TicketWidget.js
+++ src/components/TicketWidget.js
@@ -10,60 +10,74 @@ import SeatSvg from "../assets/seat-available.svg";
 const TicketWidget = () => {
   // TODO: use values from Context
   const { state, actions } = React.useContext(SeatContext);
-  // console.log(state, actions);
+  console.log(state, actions);
   const numOfRows = state.numOfRows;
   const seatsPerRow = state.seatsPerRow;
   // console.log(numOfRows, seatsPerRow);
 
   // TODO: implement the loading spinner <CircularProgress />
   // with the hasLoaded flag
+  const hasLoaded = state.hasLoaded;
 
-  return (
-    <Wrapper>
-      {range(numOfRows).map((rowIndex) => {
-        const rowName = getRowName(rowIndex);
+  if (hasLoaded === false) {
+    return (
+      <>
+        <LoadingWrapper>
+          <CircularProgress />
+        </LoadingWrapper>
+      </>
+    );
+  } else {
+    return (
+      <Wrapper>
+        {range(numOfRows).map((rowIndex) => {
+          const rowName = getRowName(rowIndex);
+          return (
+            <Row key={rowIndex}>
+              <RowLabel>Row {rowName}</RowLabel>
+              {range(seatsPerRow).map((seatIndex) => {
+                const seatId = `${rowName}-${getSeatNum(seatIndex)}`;
 
-        return (
-          <Row key={rowIndex}>
-            <RowLabel>Row {rowName}</RowLabel>
-            {range(seatsPerRow).map((seatIndex) => {
-              const seatId = `${rowName}-${getSeatNum(seatIndex)}`;
-
-              return (
-                <SeatWrapper key={seatId}>
-                  <img alt="seat image" src={SeatSvg} />
-                </SeatWrapper>
-              );
-            })}
-          </Row>
-        );
-      })}
-    </Wrapper>
-  );
+                return (
+                  <SeatWrapper key={seatId}>
+                    <img alt="seat image" src={SeatSvg} />
+                  </SeatWrapper>
+                );
+              })}
+            </Row>
+          );
+        })}
+      </Wrapper>
+    );
+  }
 };
 
 const Wrapper = styled.div`
-  background: #eee;
+  background-color: #333;
   border: 1px solid #ccc;
   border-radius: 3px;
   padding: 8px;
 `;
 
+const LoadingWrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+`;
+
 const Row = styled.div`
   display: flex;
   position: relative;
-
-  &:not(:last-of-type) {
-    border-bottom: 1px solid #ddd;
-  }
 `;
 
 const RowLabel = styled.div`
   font-weight: bold;
+  padding: 40px 30px 10px 50px;
 `;
 
 const SeatWrapper = styled.div`
   padding: 5px;
+  background: #eee;
 `;
 
 export default TicketWidget;

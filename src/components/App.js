9ce378c9add746b0dcac518eeaf6dbdf977e0092
Przemek Piwek <przemekpiwek@gmail.com>
2020-08-20 22:12:27 -0400

    Exercise 1,2,3 complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
+import { SeatContext } from "./SeatContext";
+import TicketWidget from "./TicketWidget";
 
-import GlobalStyles from './GlobalStyles';
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
+  const {
+    actions: { receiveSeatInfoFromServer },
+    state,
+  } = React.useContext(SeatContext);
+
+  React.useEffect(() => {
+    fetch("/api/seat-availability")
+      .then((res) => res.json())
+      .then((data) => receiveSeatInfoFromServer(data));
+  }, []);
+
   return (
     <>
       <GlobalStyles />
-      TODO: write code
+      <TicketWidget
+        numOfRows={state.numOfRows}
+        seatsPerRow={state.seatsPerRow}
+      />
     </>
   );
 }
--- src/components/TicketWidget.js
+++ src/components/TicketWidget.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import styled from 'styled-components';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import React from "react";
+import styled from "styled-components";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-import { getRowName, getSeatNum } from '../helpers';
-import { range } from '../utils';
+import { getRowName, getSeatNum } from "../helpers";
+import { range } from "../utils";
 
-const TicketWidget = () => {
+const TicketWidget = ({ numOfRows, seatsPerRow }) => {
+  console.log(numOfRows);
   // TODO: use values from Context
-  const numOfRows = 6;
-  const seatsPerRow = 6;
+  // const numOfRows = 6;
+  // const seatsPerRow = 6;
 
   // TODO: implement the loading spinner <CircularProgress />
   // with the hasLoaded flag
 
   return (
     <Wrapper>
-      {range(numOfRows).map(rowIndex => {
+      {range(numOfRows).map((rowIndex) => {
         const rowName = getRowName(rowIndex);
 
         return (
           <Row key={rowIndex}>
             <RowLabel>Row {rowName}</RowLabel>
-            {range(seatsPerRow).map(seatIndex => {
+            {range(seatsPerRow).map((seatIndex) => {
               const seatId = `${rowName}-${getSeatNum(seatIndex)}`;
 
               return (

fce54948651484a52ff4bb3cf178694b4aaa37c4
donnyesq <adonis@darmas.ca>
2020-08-22 12:15:53 -0400

    Seat component clean up

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -1,12 +1,7 @@
 import React from "react";
 import seatSrc from "../assets/seat-available.svg";
-import styled from "styled-components";
-
-import { SeatContext } from "./SeatContext";
 
 export const Seat = ({ isBooked }) => {
-  const { state } = React.useContext(SeatContext);
-
   return (
     <img
       src={seatSrc}

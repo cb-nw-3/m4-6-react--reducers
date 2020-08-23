e310564d62dde9c5cd478bb8e582bb11a6e49977
Roger Lam <rogerlam@wylco.org>
2020-08-23 11:49:04 -0400

    initial commit to ex 4

--- src/components/TicketWidget.js
+++ src/components/TicketWidget.js
@@ -2,16 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+import { SeatContext } from './SeatContext'
 import { getRowName, getSeatNum } from '../helpers';
 import { range } from '../utils';
+import seatSrc from "../assets/seat-available.svg";
 
 const TicketWidget = () => {
-  // TODO: use values from Context
-  const numOfRows = 6;
-  const seatsPerRow = 6;
+
+  const { state, dispatch } = React.useContext(SeatContext);
+
+  console.log(state);
+
+  const numOfRows = state.numOfRows;
+  const seatsPerRow = state.seatsPerRow;
 
   // TODO: implement the loading spinner <CircularProgress />
   // with the hasLoaded flag
+
+  const Seat = () => {
+    return <img alt="seat for booking" src={seatSrc} />
+  }
 
   return (
     <Wrapper>
@@ -20,13 +30,13 @@ const TicketWidget = () => {
 
         return (
           <Row key={rowIndex}>
-            <RowLabel>Row {rowName}</RowLabel>
+            {/* <RowLabel>Row {rowName}</RowLabel> */}
             {range(seatsPerRow).map(seatIndex => {
               const seatId = `${rowName}-${getSeatNum(seatIndex)}`;
 
               return (
                 <SeatWrapper key={seatId}>
-                  {/* TODO: Render the actual <Seat /> */}
+                  <Seat />
                 </SeatWrapper>
               );
             })}
@@ -37,20 +47,27 @@ const TicketWidget = () => {
   );
 };
 
+// GlobalStyles forces body to be 100wh
+// I don't really want that. But... eh.
+
 const Wrapper = styled.div`
   background: #eee;
   border: 1px solid #ccc;
   border-radius: 3px;
   padding: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 200px;
 `;
 
 const Row = styled.div`
   display: flex;
   position: relative;
 
-  &:not(:last-of-type) {
-    border-bottom: 1px solid #ddd;
-  }
+  // &:not(:last-of-type) {
+  //   border-bottom: 1px solid #ddd;
+  // }
 `;
 
 const RowLabel = styled.div`

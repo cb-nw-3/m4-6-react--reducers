3284741e08e3e53cff180b58b9375a31481b8ffb
Hiba Z <hiba4.z@gmail.com>
2020-08-24 23:06:06 -0400

    Workshop m4-6 first step

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-
-import GlobalStyles from './GlobalStyles';
+import React from "react";
+import { SeatContext } from "./SeatContext";
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
+  const {
+    actions: { receiveSeatInfoFromServer },
+  } = React.useContext(SeatContext);
+
+  React.useEffect(() => {
+    fetch("/api/seat-availability")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <>
       <GlobalStyles />

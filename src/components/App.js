9054d6f7382240bd239e8da5db53506281dd01a5
Truong Dao <truong.dao.do@gmail.com>
2020-08-26 20:58:26 -0400

    Solution_TD_React6

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-import GlobalStyles from './GlobalStyles';
+import { SeatContext } from "./SeatContext";
+import GlobalStyles from "./GlobalStyles";
+
+import TicketWidget from "./TicketWidget";
 
 function App() {
+  const {
+    numOfRows,
+    actions: { receiveSeatInfoFromServer },
+  } = React.useContext(SeatContext);
+
+  React.useEffect(() => {
+    fetch("/api/seat-availability")
+      .then((res) => res.json())
+      // .then((json) => console.log(json))
+      .then((data) => receiveSeatInfoFromServer(data));
+  }, []);
+
   return (
     <>
       <GlobalStyles />
-      TODO: write code
+      {/* <h1>There are {numOfRows}</h1> */}
+      <Centered>
+        <TicketWidget />
+      </Centered>
     </>
   );
 }
+
+const Centered = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default App;
--- src/components/TicketWidget.js
+++ src/components/TicketWidget.js
@@ -1,32 +1,50 @@
-import React from 'react';
-import styled from 'styled-components';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import React from "react";
+import styled from "styled-components";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Seat from "./Seat";
 
-import { getRowName, getSeatNum } from '../helpers';
-import { range } from '../utils';
+import { getRowName, getSeatNum } from "../helpers";
+import { range } from "../utils";
+import { SeatContext } from "./SeatContext";
 
 const TicketWidget = () => {
   // TODO: use values from Context
-  const numOfRows = 6;
-  const seatsPerRow = 6;
+
+  const { numOfRows, seatsPerRow, hasLoaded, seats } = React.useContext(SeatContext);
+
+
+  // const numOfRows = 6;
+  // const seatsPerRow = 6;
 
   // TODO: implement the loading spinner <CircularProgress />
   // with the hasLoaded flag
+  if (!hasLoaded) {
+    return <CircularProgress />;
+  }
 
   return (
     <Wrapper>
-      {range(numOfRows).map(rowIndex => {
+      {range(numOfRows).map((rowIndex) => {
         const rowName = getRowName(rowIndex);
 
         return (
           <Row key={rowIndex}>
             <RowLabel>Row {rowName}</RowLabel>
-            {range(seatsPerRow).map(seatIndex => {
+            {range(seatsPerRow).map((seatIndex) => {
               const seatId = `${rowName}-${getSeatNum(seatIndex)}`;
+              const seat = seats[seatId];
+              const seatNumber = getSeatNum(seatIndex);
 
               return (
                 <SeatWrapper key={seatId}>
                   {/* TODO: Render the actual <Seat /> */}
+
+                  <Seat 
+                  isBooked={seat.isBooked}
+                  price={seat.price}
+                  row={rowName}
+                  number={seatNumber}
+                  />
                 </SeatWrapper>
               );
             })}
@@ -55,6 +73,16 @@ const Row = styled.div`
 
 const RowLabel = styled.div`
   font-weight: bold;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  transform: translateX(calc(-100% - 30px));
+  font-size: 14px;
+  color: white;
+  font-weight: bold;
+  line-height: 46px;
 `;
 
 const SeatWrapper = styled.div`

a48d9763b95838a266d28e0b7d0ef5a5da861dd2
anwesh-sarkar <anwesh.sarkar@gmail.com>
2020-09-05 00:16:54 -0400

    Finished Exercise 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React from "react";
+import { SeatContext } from "./SeatContext";
 
-import GlobalStyles from './GlobalStyles';
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
+  const {
+    actions: { receiveSeatInfoFromServer },
+  } = React.useContext(SeatContext);
+
+  React.useEffect(() => {
+    fetch("/api/seat-availability", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <>
       <GlobalStyles />
